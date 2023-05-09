aff6461bacd6892ac38ba6eff3d83057a643fd0e
Nirvean <snirvean@gmail.com>
2023-05-09 22:25:44 +0200

    Added GitHub button + GitHub link

--- src/Weather.js
+++ src/Weather.js
@@ -179,6 +179,13 @@ export default function Weather() {
               </div>
       </div>*/}
           </div>
+          <div>
+            <button id="github-button" type="button" title="Open-source code by Sonia 🐚">
+                <a href="https://github.com/Nirvean/ac-weather-react" class="github-link" target="_blank" rel="noreferrer">
+                    <i class="fa-brands fa-github github-icon"></i>
+                </a>
+            </button>
+          </div>
         </div>
       );
 }
