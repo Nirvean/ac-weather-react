dbb81f8d0717f77f6dcd1683defb09f4f10b9b3a
Nirvean <snirvean@gmail.com>
2023-05-23 18:33:46 +0200

    Replaced IDs with classnames

--- src/Weather.js
+++ src/Weather.js
@@ -19,43 +19,43 @@ export default function Weather() {
     <div className="Weather">
       <div className="row first-row">
         <div className="col-9 mt-3 mb-3">
-          <form id="search-form" action="#">
+          <form className="search-form" action="#">
             <input
-              id="search-input"
+              className="search-input"
               type="text"
               placeholder="Search city"
               autoComplete="off"
             />
-            <button id="delete-button" type="reset" title="Clear">
+            <button className="delete-button" type="reset" title="Clear">
               <i className="fa-solid fa-xmark delete-icon"></i>
             </button>
             <span className="lbar">|</span>
-            <button id="search-button" type="submit" title="Search">
+            <button className="search-button" type="submit" title="Search">
               <i className="fa-solid fa-magnifying-glass search-icon"></i>
             </button>
           </form>
         </div>
         <div className="col-3 mt-3">
-          <ul id="secondary-buttons">
-            <li id="secondary-button-1">
+          <ul className="secondary-buttons">
+            <li className="secondary-button-1">
               <button
-                id="location-button"
+                className="location-button"
                 type="button"
                 title="Current location"
               >
                 <i className="fa-solid fa-location-dot location-icon"></i>
               </button>
             </li>
-            <li id="secondary-button-2">
+            <li className="secondary-button-2">
               <button
-                id="fahrenheit-celsius-button"
+                className="fahrenheit-celsius-button"
                 type="button"
                 title="Change temp unit"
               >
                 <span className="fahrenheit-symbol">ºF</span>
               </button>
             </li>
-            <li id="secondary-button-3">
+            <li className="secondary-button-3">
               <audio controls id="audio">
                 <source
                   src="https://audio.jukehost.co.uk/QeMcZD4Axh76BZWaUtZkWNhykbEJTmBI"
@@ -63,7 +63,11 @@ export default function Weather() {
                 />
                 Your browser does not support the audio element.
               </audio>
-              <button id="play-pause-button" type="button" title="Play music">
+              <button
+                className="play-pause-button"
+                type="button"
+                title="Play music"
+              >
                 <i className="fa-solid fa-play play-icon"></i>
               </button>
             </li>
@@ -71,28 +75,27 @@ export default function Weather() {
         </div>
       </div>
       <div className="row second-row">
-        <div
-          id="current-conditions"
-          className="col-6 mb-3 first-col-second-row"
-        >
-          <div id="current-max-min-temps" className="row">
-            <div id="current-max-temp" className="col-6">
+        <div className="col-6 mb-3 first-col-second-row current-conditions">
+          <div className="row current-max-min-temps">
+            <div className="col-6 current-max-temp">
               <p>Max.: {weatherData.maxTemp} ºC ↑</p>
             </div>
-            <div id="current-min-temp" className="col-6">
+            <div className="col-6 current-min-temp">
               <p>Min.: {weatherData.minTemp} ºC ↓</p>
             </div>
           </div>
           <div className="row">
             <div className="col-12">
-              <h1 id="h1">
-                <span id="current-temp">{weatherData.currentTemp} ºC</span>
+              <h1 className="h1">
+                <span className="current-temp">
+                  {weatherData.currentTemp} ºC
+                </span>
               </h1>
             </div>
           </div>
           <div className="row">
             <div className="col-12">
-              <p id="current-feels-like">
+              <p className="current-feels-like">
                 Feels like {weatherData.feelsLike} ºC
               </p>
             </div>
@@ -100,88 +103,89 @@ export default function Weather() {
           <div className="row">
             <div className="col-6">
               <i className="fa-solid fa-droplet humidity-icon"></i>
-              <span id="humidity-span">
-                <span id="current-humidity"> {weatherData.humidity}</span> %
+              <span className="humidity-span">
+                <span className="current-humidity">
+                  {" "}
+                  {weatherData.humidity}
+                </span>{" "}
+                %
               </span>
             </div>
             <div className="col-6">
               <i className="fa-solid fa-wind wind-icon"></i>
-              <span id="wind-span">
-                <span id="current-wind"> </span>
+              <span className="wind-span">
+                <span className="current-wind"> </span>
                 {weatherData.wind} km/h
               </span>
             </div>
           </div>
         </div>
-        <div
-          id="date-city-weather-column"
-          className="col-6 mb-3 second-col-second-row"
-        >
+        <div className="col-6 mb-3 second-col-second-row date-city-weather-column">
           <div className="row">
-            <span id="current-local-date">{weatherData.date}</span>
-            <span id="current-local-hour">{weatherData.hour}</span>
+            <span className="current-local-date">{weatherData.date}</span>
+            <span className="current-local-hour">{weatherData.hour}</span>
           </div>
           <div className="row">
-            <h2 id="current-city">{weatherData.city}</h2>
+            <h2 className="current-city">{weatherData.city}</h2>
           </div>
           <div className="row">
-            <i id="current-weather-icon" className="fa-solid fa-cloud-rain"></i>
+            <i className="fa-solid fa-cloud-rain current-weather-icon"></i>
           </div>
           <div className="row">
-            <p id="current-weather-text">{weatherData.description}</p>
+            <p className="current-weather-text">{weatherData.description}</p>
           </div>
         </div>
       </div>
-      <div id="forecast" className="row justify-content-around third-row">
-        <div id="forecast-day-1" className="col-2">
+      <div className="row justify-content-around third-row forecast">
+        <div className="col-2 forecast-day-1">
           <div className="row">
             <p>THU</p>
           </div>
-          <div id="forecast-day" className="row">
-            <i id="thu-icon" className="fa-solid fa-cloud-rain"></i>
-            <span id="thu-forecast">18 º | 13 º</span>
+          <div className="row forecast-day">
+            <i className="fa-solid fa-cloud-rain thu-icon"></i>
+            <span className="thu-forecast">18 º | 13 º</span>
           </div>
         </div>
-        <div id="forecast-day-2" className="col-2">
+        <div className="col-2 forecast-day-2">
           <div className="row">
             <p>FRI</p>
           </div>
-          <div id="forecast-day" className="row">
-            <i id="fri-icon" className="fa-solid fa-cloud-rain"></i>
-            <span id="fri-forecast">16 º | 9 º</span>
+          <div className="row forecast-day">
+            <i className="fa-solid fa-cloud-rain fri-icon"></i>
+            <span className="fri-forecast">16 º | 9 º</span>
           </div>
         </div>
-        <div id="forecast-day-3" className="col-2">
+        <div className="col-2 forecast-day-3">
           <div className="row">
             <p>SAT</p>
           </div>
-          <div id="forecast-day" className="row">
-            <i id="sat-icon" className="fa-solid fa-cloud-rain"></i>
-            <span id="sat-forecast">17 º | 9 º</span>
+          <div className="row forecast-day">
+            <i className="fa-solid fa-cloud-rain sat-icon"></i>
+            <span className="sat-forecast">17 º | 9 º</span>
           </div>
         </div>
-        <div id="forecast-day-4" className="col-2">
+        <div className="col-2 forecast-day-4">
           <div className="row">
             <p>SUN</p>
           </div>
-          <div id="forecast-day" className="row">
-            <i id="sunday-icon" className="fa-solid fa-sun"></i>
-            <span id="sun-forecast">18 º | 9 º</span>
+          <div className="row forecast-day">
+            <i className="fa-solid fa-sun sunday-icon"></i>
+            <span className="sun-forecast">18 º | 9 º</span>
           </div>
         </div>
-        <div id="forecast-day-5" className="col-2">
+        <div className="col-2 forecast-day-5">
           <div className="row">
             <p>MON</p>
           </div>
-          <div id="forecast-day" className="row">
-            <i id="mon-icon" className="fa-solid fa-cloud-sun"></i>
-            <span id="mon-forecast">17 º | 10 º</span>
+          <div className="row forecast-day">
+            <i className="fa-solid fa-cloud-sun mon-icon"></i>
+            <span className="mon-forecast">17 º | 10 º</span>
           </div>
         </div>
       </div>
       <div>
         <button
-          id="github-button"
+          className="github-button"
           type="button"
           title="Open-source code by Sonia 🐚"
         >
