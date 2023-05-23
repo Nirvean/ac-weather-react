9c2adb12a9df09e8bc72eb9b4dd41cb138bf7756
Nirvean <snirvean@gmail.com>
2023-05-23 17:59:02 +0200

    Formatted the code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import './App.css';
+import "./App.css";
 import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
       <div class="container mt-3 p-5">
-      <Weather />
+        <Weather />
       </div>
     </div>
   );
